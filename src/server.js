fda6f1c220f53807652ece2aab16b4498eec0903
vzhufk <vzhufk@gmail.com>
2018-02-25 12:27:10 +0200

    Initial parcels and directories.

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import Hapi from "hapi";
+
+// Create a server with a host and port
+const server = Hapi.server({
+  host: "localhost",
+  port: 8000
+});
+
+// Add the route
+server.route({
+  method: "GET",
+  path: "/hello",
+  handler: (req, resp) => {
+    return "hello world";
+  }
+});
+
+// Start the server
+async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Server running at:", server.info.uri);
+}
+
+start();
